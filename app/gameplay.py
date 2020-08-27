598cb548e2814c01c316de8124ec269ebbf51e9b
LeeSomeToNFro <34737113+LeeSomeToNFro@users.noreply.github.com>
2020-08-28 00:57:04 +0800

    init commit

---
+++ app/gameplay.py
@@ -0,0 +1,14 @@
+def deck_pool():
+
+    return []
+
+
+
+class Game:
+
+    def __init__(self,deck_pool):
+        self.deck_pool = deck_pool
+    
+    def refresh(self,end_game=False,card_spell='0'):
+        print('aaa')
+    
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,23 @@
 from app import app
-
+from flask import request,render_template
+from app.gameplay import Game,deck_pool
 @app.route('/')
 def test():
     return "test"
+
+@app.route('/game',methods=['GET','POST'])
+def gameplay():
+    if request.method=='GET':
+        game = Game(deck_pool())
+        return render_template('game.html',game=game)
+    
+    elif request.method=="POST":
+        end_round = request.form.getlist('end')
+        card_spell = request.form.getlist('card_id')
+        if end_round==True:
+            game.refresh(end_round=True)
+        else:
+            game.refresh(card_spell=card_spell)
+        
+        return render_template('game.html',game=game)
+
