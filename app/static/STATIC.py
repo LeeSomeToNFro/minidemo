0d409abc730a383021ea151ed59ab86b4754f95f
LeeSomeToNFro <34737113+LeeSomeToNFro@users.noreply.github.com>
2020-08-30 15:18:23 +0800

    0830 rebuildv0.1

--- app/static/STATIC.py
+++ app/static/STATIC.py
@@ -1,23 +1,5 @@
 import pandas as pd
-
-class Card:
-    def __init__(self,id,card_type,name="Null",description="Null",r_stock=[0,0,0,0],\
-        r_capacity=[0,0,0,0],r_production=[0,0,0,0],r_consumption=[0,0,0,0],buff_id=0,\
-            tier=1,autouse=False,human=0,human_rounds=1,counts=0):
-        self.id=id
-        self.type=card_type
-        self.name=name
-        self.description=description
-        self.r_stock=r_stock
-        self.r_capacity=r_capacity
-        self.r_production=r_production
-        self.r_consumption=r_consumption
-        self.buff_id=buff_id
-        self.tier=tier
-        self.autouse=autouse
-        self.counts=counts
-        self.human=human
-        self.human_rounds=human_rounds
+from app.models import Card
 
 def deck_pool():
     return [100101,100102,100103]
@@ -38,7 +20,7 @@ def generate_card_list(id_list):
         #print(id)
         c = data.loc[id]
         r_stock=to_list(c['r_stock'])
-        print(r_stock)
+        #print(r_stock)
         card=Card(id=id,card_type=c['type'],name=c['name'],description=c['description'],\
             human=c['human'],human_rounds=c['human_rounds'],buff_id=c['buff_id'],\
                 autouse=c['autouse'],counts=c['counts'],r_stock=to_list(c['r_stock']),\
