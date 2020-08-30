358699880dee293e725a14ef1c206babf30fba8c
LeeSomeToNFro <34737113+LeeSomeToNFro@users.noreply.github.com>
2020-08-30 17:41:25 +0800

    11

--- app/static/STATIC.py
+++ app/static/STATIC.py
@@ -13,13 +13,13 @@ def to_list(r):
     return[int(s[0]),int(s[1]),int(s[2]),int(s[3])]
 
 def generate_card_list(id_list):
-    data = pd.read_excel('app\static\cards.xlsx',sheet_name='card',index_col='id')
+    data=pd.read_excel('app\static\cards.xlsx',sheet_name='card',index_col='id')
     card_list=[]
     #print(id_list)
     for id in id_list:
         #print(id)
         c = data.loc[id]
-        r_stock=to_list(c['r_stock'])
+        #r_stock=to_list(c['r_stock'])
         #print(r_stock)
         card=Card(id=id,card_type=c['type'],name=c['name'],description=c['description'],\
             human=c['human'],human_rounds=c['human_rounds'],buff_id=c['buff_id'],\
