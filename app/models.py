56dea7649849918987a7937dd0b2a8b725a55299
LeeSomeToNFro <34737113+LeeSomeToNFro@users.noreply.github.com>
2020-09-01 17:29:34 +0800

    config v0.1

--- app/models.py
+++ app/models.py
@@ -1,18 +1,51 @@
+import pandas as pd
+
 class Card:
-    def __init__(self,id,card_type,name="Null",description="Null",r_stock=[0,0,0,0],\
-        r_capacity=[0,0,0,0],r_production=[0,0,0,0],r_consumption=[0,0,0,0],buff_id=0,\
-            tier=1,autouse=False,human=0,human_rounds=1,counts=0):
+    def __init__(self,id,type_card,human,name="null",description="null",human_rounds=1,autouse=False,effect_id=[],effect_param=[]):
         self.id=id
-        self.type=card_type
-        self.name=name
-        self.description=description
-        self.r_stock=r_stock
-        self.r_capacity=r_capacity
-        self.r_production=r_production
-        self.r_consumption=r_consumption
-        self.buff_id=buff_id
-        self.tier=tier
-        self.autouse=autouse
-        self.counts=counts
+        self.type=type_card
         self.human=human
         self.human_rounds=human_rounds
+        self.name=name
+        self.description=description
+        self.autouse=autouse
+        self.effect_id=effect_id
+        self.effect_param=effect_param
+        
+
+class City:
+    def __init__(self):
+        self.resource={}
+        rc=pd.read_excel('app\static\config.xlsx',sheet_name='resource')
+        for i in range(len(rc)):
+            c = rc.iloc[i]
+            self.resource[c['id']]=c['value']
+    
+    def endround_rc(self):
+        self.resource[100]+=(self.resource[102]-self.resource[103])
+        self.resource[200]+=(self.resource[202]-self.resource[203])
+        self.resource[300]+=(self.resource[302]-self.resource[303])
+        self.resource[400]+=(self.resource[402]-self.resource[403])
+        [self.resource[100],self.resource[200],self.resource[300],self.resource[400]] =self.inbound(\
+            [self.resource[100],self.resource[200],self.resource[300],self.resource[400]],\
+                [self.resource[101],self.resource[201],self.resource[301],self.resource[401]])
+
+    
+    def inbound(self,a,b):
+        result=[]
+        for i in range(len(a)):
+            temp = a[i]
+            if a[i]<0:
+                temp=0
+            if a[i]>b[i]:
+                temp=b[i]
+            result.append(temp)
+        return result
+
+
+
+        
+
+if __name__=="__main__":
+    c=City()
+    print(c.resource)
